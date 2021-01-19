972d1e2c8b124e6a65a4f0a6d9af0fd503141dbd
Azure Pipeline <trevar.ladd@oneamerica.com>
2021-01-18 20:54:33 -0500

    Converted to static html

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return <div className="footer">This is the footer</div>;
-};
-
-export default Footer;
--- src/components/Landing.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Landing extends Component {
-  componentDidMount() {}
-  render() {
-    return <div>this is the main landing code</div>;
-  }
-}
-export default Landing;
